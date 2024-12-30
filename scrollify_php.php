ee7e3fad7785c553fac94003402d25df76cf3998
EMMAN <emmanuelbarrameda1@gmail.com>
2024-12-30 21:37:10 +0800

    123024 937PM Initial commit

---
+++ scrollify_php.php
@@ -0,0 +1,29 @@
+<!-- /* =========================================================
+
+    SCROLLIFY-PHP - SCROLL PROGRESS INDICATOR WHEN SCROLLING ON WEB PAGE
+    https://github.com/emmanpbarrameda/scrollify-php
+
+    based on https://github.com/emmanpbarrameda/scrollify-js
+
+    © emmanpbarrameda - https://emmanpbarrameda.github.io/
+
+============================================================ */ -->
+
+<!-- ==================== HOW TO USE ==================== -->
+<!-- 
+    To integrate the scroll progress bar into your webpage, include the following PHP code:
+    <?php include 'scrollify_php.php' ?>
+    This will add the necessary HTML, CSS, and JavaScript for the scroll progress bar.
+-->
+
+<div 
+    class="scrollify_scroll_progress" 
+    data-height="4px"
+    data-background="linear-gradient(to left, #B374F8, #4da3ff)" 
+    data-z-index="999" 
+    data-top="89px">
+</div>
+
+<!-- change this, depends on asset path -->
+<link rel="stylesheet" href="assets/css/scrollify-scrollprogress-indicator.css?v=1.1.0">
+<script src="assets/js/scrollify-scrollprogress-indicator.js?v=1.1.0"></script>
